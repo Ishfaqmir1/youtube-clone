498124958946c22ac0f9e889fadc1029aa1fc5d3
ishfaqmir1 <mirishfaq01@gmail.com>
2025-09-25 09:22:31 +0530

    updated App.jsx with updated routes and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,49 @@
-import React from 'react'
-import{Routes,Route} from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import Home from './pages/Home';
-import Trending from './pages/Trending';
-import Subscriptions from './pages/Subscriptions';
-import VideoDetail from './pages/VideoDetails';
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
+import Trending from "./pages/Trending";
+import Subscriptions from "./pages/Subscriptions";
+import VideoDetail from "./pages/VideoDetails";
+import Search from "./pages/Search";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import Upload from "./pages/Upload";
 
 function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [user, setUser] = useState(null);
+
+  // Load user from localStorage on app start
+  useEffect(() => {
+    const savedUser = localStorage.getItem("user");
+    if (savedUser) {
+      setUser(JSON.parse(savedUser));
+    }
+  }, []);
+
   return (
-      <div className="flex flex-col h-screen">
-      <Navbar />
+    <div className="flex flex-col h-screen">
+      <Navbar
+        onToggleSidebar={() => setSidebarOpen(!sidebarOpen)}
+        user={user}
+        setUser={setUser}
+      />
       <div className="flex flex-1">
-        <Sidebar />
+        <Sidebar isOpen={sidebarOpen} />
         <main className="flex-1 p-4 bg-gray-100 overflow-y-auto">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/trending" element={<Trending />} />
             <Route path="/subscriptions" element={<Subscriptions />} />
             <Route path="/video/:id" element={<VideoDetail />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/login" element={<Login setUser={setUser} />} />
+            <Route path="/register" element={<Register setUser={setUser} />} />
+            <Route path="/profile" element={<Profile user={user} />} />
+            <Route path="/upload" element={<Upload user={user} />} />
           </Routes>
         </main>
       </div>
@@ -26,4 +51,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
