23628b74f6c9c2d17bea8b92bd5c9bd25ef20d58
ishfaqmir1 <mirishfaq01@gmail.com>
2025-10-11 20:15:13 +0530

    Initialized frontend with Tailwind CSS, App structure, and basic styling

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: {
-    extend: {},
-  },
+// tailwind.config.js
+export default {
+  darkMode: "class",
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: { extend: {} },
   plugins: [],
-}
+};
